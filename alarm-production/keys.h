e6623559351b61df46781c92b696f3a8034d218c
ThePetrovich <petroff.ap@gmail.com>
2020-09-16 15:22:58 +0900

    Working on eeprom

--- alarm-production/keys.h
+++ alarm-production/keys.h
@@ -1,6 +1,6 @@
 #ifndef _KEYS_H_
 #define _KEYS_H_
 uint32_t wg_authorized_keys[] = {0x69EE84}; //Debug key
-char wg_names[][16] = {"Debug1"};
+char wg_names[][32] = {"Debug1"};
 const uint8_t wg_authorized_keys_num = 1;
 #endif
--- alarm-production/rfid.cpp
+++ alarm-production/rfid.cpp
@@ -1,14 +1,60 @@
 #include "alarm.h"
 #include "keys.h"
 #include "devices.h"
+#include "commands.h"
 #include <Wiegand.h>
 #include <String.h>
 
+#define EEPROM_MAGIC 0xDEADC0DE
+
 extern WIEGAND wg;
+
+struct key {
+    uint32_t id;
+    char name[20];
+    struct key *next;
+};
+
+struct eeprom_header {
+    uint32_t magic;
+    uint8_t stored_keys;
+    uint8_t flags;
+};
+
+struct key_eeprom {
+    uint32_t id;
+    char name[20];
+};
+
+struct key *list_head;
+
+int wiegand_updateHeader()
+{
+    
+}
+
+int wiegand_searchKey()
+{
+
+}
+
+struct key wiegand_readKey(uint16_t addr)
+{
+
+}
+
+int wiegand_writeKey(uint16_t addr, struct key data)
+{
+    
+}
 
 void wiegand_setup() 
 {
     wg.begin(WIEGAND_DATA1_PIN, WIEGAND_DATA2_PIN);
+    cmdparser_registerCommand((char*)"wg_add_key", wiegand_cmdAddKey);
+    cmdparser_registerCommand((char*)"wg_delete_key", wiegand_cmdDeleteKey);
+    cmdparser_registerCommand((char*)"wg_find_key", wiegand_cmdFindKey);
+    cmdparser_registerCommand((char*)"wg_find_name", wiegand_cmdFindName);
 }
 
 void wiegand_processCommand() 
@@ -46,4 +92,24 @@ void wiegand_processCommand()
 	}
     pinMode(DOOR_OPEN_PIN, INPUT);
     digitalWrite(DOOR_OPEN_PIN, LOW);
+}
+
+void wiegand_cmdAddKey()
+{
+
+}
+
+void wiegand_cmdDeleteKey()
+{
+    
+}
+
+void wiegand_cmdFindKey()
+{
+    
+}
+
+void wiegand_cmdFindName()
+{
+    
 }
